01d7cb15bde178daa128b221606a1e9c28b05079
totogame1010 <145981046+totogame1010@users.noreply.github.com>
2025-05-05 16:31:50 +0900

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// scripts.js
+
+// スムーズスクロール
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
